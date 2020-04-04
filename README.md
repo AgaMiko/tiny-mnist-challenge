2ce873c44b71bc66cf46c95b2e7d485a9ba493aa
AgaMiko <agnieszka.mikolajczyk.pg@gmail.com>
2020-04-04 21:47:21 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TinyMNISTChallenge
- Tiny MNIST Challenge for university student's class
+Tiny MNIST Challenge for Gdańsk University student's class.
+
+### Matlab examples - code and toolboxes instruction.
+Built-in data loaders and submission preparation.
+* Simple neural network - [code]('matlab_examples/simple_Network.m')
+A framework for using matlab toolbox to design basic fully-connected network. 
+
+* Basic convolutional neural network - [code]('matlab_examples/simple_CNN.m')
+Simple neural network with 2 convolutional layers, relu activation, maxpooling and fully-connected layer.
+
+### Pytorch examples - ready to run in Colab
+Built-in data loaders, submission preparation and sending.
+
+* Simple neural network - [code]('pytoch_examples/simple_Network.m')
+This is a basic fully-connected network. Two fully-connected layers and relu activation.
+
+* Basic convolutional neural network - [code]('pytorch_examples/simple_CNN.m')
+Simple neural network with 2 convolutional layers, relu activation, maxpooling and fully-connected layer.
---
+++ matlab_examples/simple_Network.m
@@ -0,0 +1,85 @@
+% MSI TinyMNIST CHallenge 2020
+% Agnieszka Miko³ajczyk
+% Use "Run section" or "Run and Advance" option!
+%% Load Dataset
+clc, clear
+% Load images
+images=[];
+filenames=[];
+labels=[];
+idx=0
+disp('Loading...')
+for folder=1:10
+    directory="TinyMNIST/train/"+(folder-1);
+    fileList = fullfile(directory, '*.jpg');
+    jpegFiles = dir(fileList);
+    folder
+    for image=1:size(jpegFiles,1)
+        idx=idx+1;
+        % consider resizing images to a smaller size
+        % resize it here, before reshaping 
+        images = [images,reshape(imread(fullfile(jpegFiles(image).folder,jpegFiles(image).name)),28*28,[])]; 
+        % reshape image from 28x28 to a single vector 28*28 = 784
+        labels(idx)=folder;
+    end
+    
+end
+images=double(images); %convert it to double so we can use nftool
+%% Network architecture
+nftool
+% generate your own network with: APPS -> Neural Net Fitting. (or type nftool) 
+% windows pops click "Next"
+% Load "images" as inputs and "labels" as Targets
+% go next, select parameters as you wish
+% train algorithm with "Train" button
+% after traning click "Next" few times to the last page
+% select "Simple Script" or "Advanced Script"
+% copy code here
+
+
+
+
+% YOUR CODE
+
+
+
+
+
+
+%% Calculate accuracy
+predictions = xxx; 
+labels = xxx
+accuracy = sum(predictions == labels)/numel(labels)
+%% Load test images
+images=[];
+filenames=[];
+labels=[];
+idx=0
+disp('Loading...')
+directory="TinyMNIST/test/test/";
+fileList = fullfile(directory, '*.jpg');
+tinyMNIST_test = dir(fileList);
+for image=1:size(tinyMNIST_test,1)
+    idx=idx+1;
+    images = [images,reshape(imread(fullfile(tinyMNIST_test(image).folder,tinyMNIST_test(image).name)),28*28,[])];
+    labels(idx)=folder;
+end
+  
+images=double(images); %convert it to double so we can use nftool
+
+%% Predict for test data
+
+predictions = net(images) % net - your network
+
+%% Prepare submission file
+submission=[]
+for record=1:600
+    filename=split(tinyMNIST_test(record).name,'_'); % split string with _ delimiter
+    filename=split(filename{2},'.'); % take second part of the string and split with . delimiter
+    index = filename{1};
+    submission = [submission; index, predictions(record)];
+end
+%%  open submission variable and copy it to excel
+% add header 'id' and 'category' and save to csv file
+openvar('submission')
+% congratulations! now you can save the file
